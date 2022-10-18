80d24fb83c46e0e7071062ca09be3f09585631ac
Dorian Majano <dorian.majano@gmail.com>
2022-10-18 14:26:06 -0700

    Add app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const db = require("./db");
+const { DestinationRoutes } = require("./routes");
+
+app.get("/", (req, res) => {
+  db.collection("destinations")
+    .find()
+    .toArray()
+    .then((results) => res.send(results))
+    .catch((e) => console.error("We have an error: ", e));
+});
+
+app.use("/api", DestinationRoutes);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on PORT ${process.env.PORT}`)
+);
