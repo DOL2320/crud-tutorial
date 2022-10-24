53f39e2cca721d985ef4b4c2bdfdbe7ceda4a797
Dorian Majano <dorian.majano@gmail.com>
2022-10-24 15:21:51 -0700

    Update connection URI

--- db/index.js
+++ db/index.js
@@ -2,14 +2,11 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-  .connect(
-    "mongodb+srv://lando:W5XY6HMFpEV3nieA@cluster0.bgsgsrd.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      // directConnection: true,
-    }
-  )
+  .connect(process.env.MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    // directConnection: true,
+  })
   .then(() => console.log("Database connected"))
   .catch((e) => console.error("Database not connected:", e));
 
